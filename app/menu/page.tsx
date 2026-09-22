c8d4657cc1caadbae251dc560ef99a383cf61394
mikonzi33 <efeyilman18@gmail.com>
2026-09-22 20:37:48 +0200

    Update QR menu image

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -6,7 +6,7 @@ export default function MenuPage() {
       <section className="mx-auto flex max-w-4xl justify-center px-4 pb-16 pt-32 sm:px-6">
         <div className="rounded-[32px] border border-[#f4d9df] bg-[#fffaf4] p-3 shadow-[0_18px_40px_rgba(96,65,42,0.1)]">
           <img
-            src="/menu-qr.jpg"
+            src="/menu-qr.png"
             alt="Kitty Cafe QR menu"
             className="w-full max-w-[680px] rounded-[24px] object-contain"
           />
